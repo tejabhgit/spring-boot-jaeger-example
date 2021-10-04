e37c02663dc3dfba5882bc9b6427859a4adbb5da
satadru555 <satadru1991@gmail.com>
2021-10-05 02:48:51 +0530

    Initial Commit

--- jaeger-client/src/main/java/com/rps/jaegerclient/JaegerClientApplication.java
+++ jaeger-client/src/main/java/com/rps/jaegerclient/JaegerClientApplication.java
@@ -3,6 +3,7 @@ package com.rps.jaegerclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JaegerClientApplication {
 
--- jaeger-server/src/main/java/com/rps/jaegerserver/service/JaegerServerService.java
+++ jaeger-server/src/main/java/com/rps/jaegerserver/service/JaegerServerService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+
 @Service
 public class JaegerServerService {
 
